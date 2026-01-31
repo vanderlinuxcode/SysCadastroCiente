a17a5c64ca1ec7021881453affa5d018f2e83f49
vanderli <vanderlitecnologia@gmail.com>
2026-01-31 00:21:13 -0300

    Projeto Crud Cadastro v1

---
+++ CrudCadastro.V1/SistemaClientes/src/controller/BCrypt.java
@@ -0,0 +1,9 @@
+// Classe utilitária para verificação de senhas com BCrypt
+package controller;
+
+public class BCrypt {
+
+	public static boolean checkpw(String senha, String senhaCriptografada) {
+		return false;
+	}
+}
---
+++ CrudCadastro.V1/SistemaClientes/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SistemaClientes {
+	requires jdk.jdi;
+	requires java.sql;
+    requires java.desktop; // necessário para javax.swing, java.awt, etc.
+}
